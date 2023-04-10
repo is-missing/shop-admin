97594f638d4e031d568d6133d16f1abff9763583
小李不见了 <11722237+xiao-li-is-missing@user.noreply.gitee.com>
2023-04-10 18:36:40 +0800

    feat(global): 配置vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // 打包白屏问题
+  publicPath: './',
+  // 打包默认文件
+  outputDir: 'build',
+  transpileDependencies: true,
+  lintOnSave: false,
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        views: '@/views'
+      }
+    }
+  },
+  devServer: {
+    // port: '8888'
+    host: 'localhost',
+    open: true
+  }
 })
